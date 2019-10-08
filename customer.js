cf84d91407ca37c10055f87fe6de5ecd1630690b
tardyparty <tardyparty@users.noreply.github.com>
2019-10-08 16:01:02 -0500

    displays products for sale

--- customer.js
+++ customer.js
@@ -13,5 +13,24 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("Connected as ID " + connection.threadId);
+    
+    // display items initilly
+    showItems();
 })
+
+
+// displays all items for sale on startup
+function showItems() {
+
+    // query all products from bamazon database
+    connection.query("SELECT * FROM products", function(err, res) {
+
+        if (err) throw err;
+
+        // display all items for sale
+        for (var i = 0; i < res.length; i++) {
+            var product = res[i];
+            console.log("ID: " + product.item_id, "Product: " + product.product_name, "Price: " + product.price + "\n")
+        }
+    })
+}
