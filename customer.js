b08203b8b944f6b92d6d64b39656c89272393a84
tardyparty <tardyparty@users.noreply.github.com>
2019-10-08 22:13:46 -0500

    start of manager.js app

--- customer.js
+++ customer.js
@@ -1,7 +1,6 @@
+// requirements
 var inquirer = require('inquirer');
 var mysql = require('mysql');
-
-
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -11,6 +10,14 @@ var connection = mysql.createConnection({
     database: "bamazon",
 });
 
+// global variables
+var products; 
+var item;
+var num;
+var newQuantity;
+
+
+// app logic
 connection.connect(function(err) {
     if (err) throw err;
     
@@ -27,10 +34,81 @@ function showItems() {
 
         if (err) throw err;
 
+        // save products as a global variable
+        products = res;
+
         // display all items for sale
         for (var i = 0; i < res.length; i++) {
             var product = res[i];
             console.log("ID: " + product.item_id, "Product: " + product.product_name, "Price: " + product.price + "\n")
         }
     })
+
+    // connection.end();
+
+    // ask what they want to buy
+    promptSale();
+}
+
+
+// asks customer what they would like to buy and makes sale
+function promptSale() {
+
+    // asks questions and saves answers
+    inquirer.prompt([
+        {
+            type: "number",
+            name: "saleID",
+            message: "What is the ID of the item you would like to buy?",
+        },
+        {
+            type: "number",
+            name: "quantity",
+            message: "How many would you like to buy?",
+        }
+    ]).then( function(answers) {
+
+        // save inputs as global variable
+        item = answers.saleID;
+        num = answers.quantity;
+
+        // check inventory to see if there is enough of the item for sale
+        checkQuantity();
+    })
+}
+
+
+// finds requested item from database
+function checkQuantity() {
+
+    connection.query("SELECT * FROM products WHERE item_id = ?", [item], function(err, res) {
+
+        // var total = res[0].price * num
+
+        if (res[0].stock_quantity < num) {
+            console.log("Sorry, we do not have enough " + res[0].product_name + " in stock.");
+            connection.end();
+        }
+        else {
+            console.log("Your total is $" + (res[0].price * num) + ". Thank you for your purchase.");
+
+            // save new quantity
+            newQuantity = res[0].stock_quantity - num;
+            updateInventory();
+            updateSales();
+        }
+    })
+
+    // connection.end();
+}
+
+
+function updateInventory() {
+
+    connection.query("UPDATE products SET stock_quantity = ? WHERE item_id = ?", [newQuantity, item]);
+}
+
+function updateSales() {
+    console.log("this is for supervisor view. update sales for department.");
+    connection.end();
 }
--- manager.js
+++ manager.js
@@ -13,5 +13,70 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("Connected as ID " + connection.threadId);
+    
+    showMenu();
 })
+
+
+function showMenu() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "action",
+            message: "Please select from the options below: ",
+            choices: [
+                "View Products",
+                "View Low Inventory",
+                "Add to Inventory",
+                "Add New Product"
+            ]
+        }
+    ]).then(function(answers) {
+
+        // perform proper function based on response
+        switch(answers.action) {
+            case "View Products":
+                viewProducts();
+                break;
+            
+            case "View Low Inventory": 
+                lowInventory();
+                break;
+
+            case "Add to Inventory":
+                addInventory();
+                break;
+
+            case "Add New Product":
+                addProduct();
+        }
+    })
+}
+
+
+// view all products
+function viewProducts() {
+
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+
+        for (var i = 0; i < res.length; i++) {
+
+            var products = res[i];
+
+            console.log(
+                "\n" + "Item_id: " + products.item_id,
+                "Product_name: " + products.product_name, 
+                "Product_Price: " + products.product_price, 
+                "Stock_Quantity: " + products.stock_quantity
+                );
+        }
+    })
+}
+
+
+// view items with low inventory (> 20 units)
+function lowInventory() {
+
+
+}
